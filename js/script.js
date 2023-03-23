ddb290b45a7366030f74883fc07a56297910ab1e
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-03-23 17:24:47 +0000

    linter fixes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Olivia B-R All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia B-R
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById("area").innerHTML = "<p>The area is = " + (3 * 5) + " cm²</p>"
+  document.getElementById("perimeter").innerHTML = "<p>The perimeter is = " + (3 + 3 + 5 + 5) + " cm </p>"  
+}
